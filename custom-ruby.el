8b37de6494fa3e012877c7ed7fe6303a61c73630
Duncan Mac-Vicar P <dmacvicar@suse.de>
2008-09-15 14:08:00 +0200

    disable ruby electric, that fixes syntax highlighting

--- custom-ruby.el
+++ custom-ruby.el
@@ -2,21 +2,18 @@
 (autoload 'ruby-mode "ruby-mode" "Major mode for editing ruby scripts." t)
 (add-to-list 'auto-mode-alist  '("\\.rb$" . ruby-mode))
 (add-to-list 'auto-mode-alist  '("\\.rhtml$" . html-mode))
-(setq interpreter-mode-alist
-  (cons '("ruby" . ruby-mode)
-  interpreter-mode-alist))
+(add-to-list 'interpreter-mode-alist '("ruby" . ruby-mode))
 
 ; indent on newline
 (add-hook 'ruby-mode-hook (lambda () (local-set-key "\r" 'newline-and-indent)))
 
 ; electric features
-(require 'ruby-electric)
+;;(require 'ruby-electric)
 
 (defun my-ruby-mode-hook ()
   (font-lock-mode t)
   (setq standard-indent 2)
-  (ruby-electric-mode t)
+;;  (ruby-electric-mode t)
   (define-key ruby-mode-map "\C-c\C-a" 'ruby-eval-buffer))
 (add-hook 'ruby-mode-hook 'my-ruby-mode-hook)
-
 (add-hook 'ruby-mode-hook 'turn-on-font-lock)
