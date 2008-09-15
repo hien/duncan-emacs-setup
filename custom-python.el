d5c9d208010214e9c191f5ecfc45fddd9e34131e
Duncan Mac-Vicar P <dmacvicar@suse.de>
2008-09-15 14:45:15 +0200

    fix python-mode

--- custom-python.el
+++ custom-python.el
@@ -1,3 +1,5 @@
+;; add python mode to the path
+(setq load-path (cons (expand-file-name "~/.emacs.d/python-mode") load-path))
 (autoload 'python-mode "python-mode" "Python Mode." t)
 (add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
 (add-to-list 'interpreter-mode-alist '("python" . python-mode))
