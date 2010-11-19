37958a75c3e3d97d0a0afe76cdd5b800d333afdb
Duncan Mac-Vicar P <dmacvicar@suse.de>
2010-11-19 13:45:17 +0100

    Move autocomplete to a separate file and disable it for now

--- custom.el
+++ custom.el
@@ -16,24 +16,10 @@
 ; activate pastie
 (require 'pastie)
 
-;(require 'auto-complete)
-;(ac-config-default)
-;(setq ac-auto-start nil)
-;(setq ac-quick-help-delay 0.5)
-;(ac-set-trigger-key "TAB")
-;(define-key ac-mode-map  [(control tab)] 'auto-complete)
-;(define-key ac-mode-map  [(control tab)] 'auto-complete)
-;(defun my-ac-config ()
-;  (setq-default ac-sources '(ac-source-abbrev ac-source-dictionary ac-source-words-in-same-mode-buffers))
-;  (add-hook 'emacs-lisp-mode-hook 'ac-emacs-lisp-mode-setup)
-;  (add-hook 'c-mode-common-hook 'ac-cc-mode-setup)
-;  (add-hook 'ruby-mode-hook 'ac-ruby-mode-setup)
-;  (add-hook 'css-mode-hook 'ac-css-mode-setup)
-;  (add-hook 'auto-complete-mode-hook 'ac-common-setup)
-;  (global-auto-complete-mode t))
-;(my-ac-config)
+; auto completion
+;(load "custom-autocomplete")
 
-; syntax check
+; automatic syntax check
 (require 'flymake)
 ;(setq flymake-log-level 3)
 
